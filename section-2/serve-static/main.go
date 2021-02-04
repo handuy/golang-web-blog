3185177c2d2aab063a75a6a9954aaf295ac442c6
handuy1992 <handuy1992@gmail.com>
2021-02-04 09:24:54 +0700

    serve static files

---
+++ section-2/serve-static/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/kataras/iris/v12"
+)
+
+func main() {
+	app := iris.Default()
+	tmpl := iris.HTML("./view", ".html")
+	app.RegisterView(tmpl)
+
+	app.HandleDir("/static", "./static")
+
+	app.Get("/posts", func(ctx iris.Context) {
+		ctx.View("posts/index.html")
+	})
+
+	app.Listen(":8080")
+}
